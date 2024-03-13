cc377a8129b0ca3450d895cc9a9f22d7fe77d4f5
MPIT0T <mathispitot@gmail.com>
2024-03-13 15:22:41 +0100

    defined error macros

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: mpitot <mpitot@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:18:47 by mbrousse          #+#    #+#             */
-/*   Updated: 2024/03/13 14:42:12 by mbrousse         ###   ########.fr       */
+/*   Updated: 2024/03/13 15:19:19 by mpitot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,21 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+
+# define EM_MALLOC "\033[0;31mError:\n\t\033[0mmalloc failed\n"
+# define EM_FORK "\033[0;31mError:\n\t\033[0mfork failed\n"
+# define EM_PIPE "\033[0;31mError:\n\t\033[0mpipe failed\n"
+# define EM_EXECVE "\033[0;31mError:\n\t\033[0mexecve failed\n"
+# define EM_OPEN "\033[0;31mError:\n\t\033[0mopen failed\n"
+# define EM_CLOSE "\033[0;31mError:\n\t\033[0mclose failed\n"
+# define EM_DUP2 "\033[0;31mError:\n\t\033[0mdup2 failed\n"
+# define EM_WAIT "\033[0;31mError:\n\t\033[0mwait failed\n"
+# define EM_SIGNAL "\033[0;31mError:\n\t\033[0msignal failed\n"
+# define EM_ARGS "\033[0;31mError:\n\t\033[0mtoo much arguments\n"
+
+# define EV_CNF 127
+# define EV_OK 0
+
 
 int		g_error = 0;
 
