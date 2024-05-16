8c7b6fb3d84252ba17456e4dfc80939cd76fb531
maxdegers <maxdegers@gmail.com>
2024-05-16 21:59:02 +0200

    added

--- srcs/execution/ft_cd.c
+++ srcs/execution/ft_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpitot <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:39:30 by mpitot            #+#    #+#             */
-/*   Updated: 2024/05/13 15:11:56 by mpitot           ###   ########.fr       */
+/*   Updated: 2024/05/16 21:46:49 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,10 +70,10 @@ void	ft_cd(t_block *block, t_data *data)
 			g_error = 1;
 			return ;
 		}
-        path = env->value;
+		path = env->value;
 	}
 	else
-        path = tmp->args[1];
+		path = tmp->args[1];
 	if (chdir(path) == -1)
 	{
 		ft_printf_fd(2, "minishell: cd: %s: No such file or directory\n", path);
