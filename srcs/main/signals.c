3e733870c2460f7d0c35cf76ce85d99d6cda49f0
maxdegers <maxdegers@gmail.com>
2024-05-20 00:42:54 +0200

    added

--- srcs/main/signals.c
+++ srcs/main/signals.c
@@ -6,7 +6,7 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:17:27 by mpitot            #+#    #+#             */
-/*   Updated: 2024/05/19 23:51:39 by mbrousse         ###   ########.fr       */
+/*   Updated: 2024/05/20 00:41:06 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 void	ft_set_signal(t_SIG_MODE mode)
 {
 	if (mode == SIG_MAIN)
+	{
+		signal(SIGQUIT, &ft_handle_sig);
 		signal(SIGINT, &ft_handle_sig);
+	}
 	else if (mode == SIG_CHILD)
+	{
+		signal(SIGQUIT, &ft_handle_sig_child);
 		signal(SIGINT, &ft_handle_sig_child);
-	signal(SIGQUIT, &ft_handle_sig);
+	}
 }
 
 void	ft_handle_sig(int sig)
@@ -41,7 +46,11 @@ void	ft_handle_sig(int sig)
 void	ft_handle_sig_child(int sig)
 {
 	if (sig == SIGINT)
-	{
 		g_error = 130;
+	if (sig == SIGQUIT)
+	{
+		ft_putstr_fd("Quit (core dumped)\n", 2);
+		rl_on_new_line();
+		rl_replace_line("", 0);
 	}
 }
